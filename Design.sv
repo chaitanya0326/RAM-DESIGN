dd8de350394410b4742658c66aa9da8013fc95b8
chaitanya0326 <chaitanyalokesh3@gmail.com>
2025-06-03 12:18:09 +0530

    Create Design.sv

---
+++ Design.sv
@@ -0,0 +1,21 @@
+module SyncRAM #(
+    parameter DATA_WIDTH = 8,
+    parameter ADDR_WIDTH = 4
+)(
+    input                  clk,
+    input                  we,
+    input  [ADDR_WIDTH-1:0] addr,
+    input  [DATA_WIDTH-1:0] din,
+    output reg [DATA_WIDTH-1:0] dout
+);
+
+    reg [DATA_WIDTH-1:0] mem [0:(1<<ADDR_WIDTH)-1];
+
+    always @(posedge clk) begin
+        if (we)
+            mem[addr] <= din;
+        else
+            dout <= mem[addr];
+    end
+
+endmodule
